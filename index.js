5376f57908022f3e060879adefbd276cb9042c78
Leonardo Faleiros <leonardo.faleiros@vsip.com.br>
2019-11-22 10:22:12 -0300

    22-11-2019

---
+++ index.js
@@ -0,0 +1,88 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projetos = [];
+
+function verificaProjetos(req, res, next) {
+  const { id } = req.params;
+  const projeto = projetos.find(p => p.id == id);
+
+  if(!projeto) {
+    return res.status(400).json({  error: 'Não existe nenhum projeto com este id.' });
+  }
+
+  return next();
+}
+
+function verificaBodyRequest(req, res, next) {
+  if(!req.body.id) {
+    return res.status(400).json({ error: 'Por favor, informe o ID do projeto' });
+  }
+
+  if(!req.body.title) {
+    return res.status(400).json({ error: 'Por favor, informe o Titulo do projeto' });
+  }
+
+  return next();
+}
+
+server.get('/projetos', (req, res) => {
+  return res.json(projetos);
+})
+
+
+server.post('/projetos', verificaBodyRequest, (req, res) => {
+  const { id, title } = req.body;
+
+  const projeto = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projetos.push(projeto);
+
+  return res.json(projeto);
+});
+
+
+server.put('/projetos/:id', verificaProjetos, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const projeto = projetos.find(p => p.id == id);
+
+  projeto.title = title;
+
+  return res.json(projeto);
+});
+
+
+server.delete('/projetos/:id', verificaProjetos, (req, res) => {
+  const { id } = req.params;
+  
+  const ProjetoID = projetos.findIndex(p => p.id == id);
+
+  projetos.splice(ProjetoID, 1);
+
+  return res.send('Projeto excluido com sucesso.');
+});
+
+
+server.post('/projetos/:id/tasks', verificaProjetos, (req, res) => {
+  const { id } = req.params;
+
+  const { title } = req.body;
+
+  const projeto = projetos.find(p => p.id == id);
+
+  projeto.tasks.push(title);
+
+  return res.json(projeto);
+});
+
+
+server.listen(4000);
